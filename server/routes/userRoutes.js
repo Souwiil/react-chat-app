a680fb778d9840a2ac64fff6f32d44b683f3f084
Souwiil <souhailbarktou1997@gmail.com>
2023-07-10 17:59:19 +0200

    test 5

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,10 +3,10 @@ const verifyToken = require('../middlewares/auth');
 
 const router = require('express').Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register",register);
+router.post("/login",login);
 router.post("/avatar/:id",avatar);
-router.get("/allusers/:id",verifyToken,getAllUsers);
+router.get("/allusers/:id",getAllUsers);
 
 
 module.exports = router;
