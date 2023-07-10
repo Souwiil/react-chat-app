ffe393614df3c71939f962211aa6393f8c92c49c
Souwiil <souhailbarktou1997@gmail.com>
2023-07-10 17:24:30 +0200

    jwt 2

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const messagesRoute = require("./routes/messagesRoute");
 const app = express();
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://react-chat-app-pink-ten.vercel.app",
+    credentials: true
+  }));
 app.use(express.json());
 
 app.use("/api/auth",userRoutes)
